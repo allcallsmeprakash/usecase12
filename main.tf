8ec78f5f76392a890e6dd65e357a41849b55a126
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-05 12:24:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -128,6 +128,14 @@ resource "aws_cognito_user_pool_domain" "user_pool_domain" {
   user_pool_id = aws_cognito_user_pool.user_pool.id
 }
 
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+
 # IAM Role for GitHub OIDC
 data "aws_caller_identity" "current" {}
 
