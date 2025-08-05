eaff947dc881cf7fd148e0154f097fea0a98f10a
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-05 14:22:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -130,3 +130,78 @@ resource "aws_cognito_user_pool_domain" "user_pool_domain" {
   domain       = "hello-world-app-prod-domain"
   user_pool_id = aws_cognito_user_pool.user_pool.id
 }
+
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "hello-world-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid    = ""
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "hello_world" {
+  function_name = "hello-world-function"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
+
+  filename         = "${path.module}/lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/lambda.zip")
+}
+
+
+resource "aws_api_gateway_rest_api" "hello_api" {
+  name        = "hello-world-api"
+  description = "API for Hello World Lambda"
+}
+
+resource "aws_api_gateway_resource" "hello_resource" {
+  rest_api_id = aws_api_gateway_rest_api.hello_api.id
+  parent_id   = aws_api_gateway_rest_api.hello_api.root_resource_id
+  path_part   = "hello"
+}
+
+resource "aws_api_gateway_method" "hello_method" {
+  rest_api_id   = aws_api_gateway_rest_api.hello_api.id
+  resource_id   = aws_api_gateway_resource.hello_resource.id
+  http_method   = "GET"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.cognito_auth.id
+}
+
+resource "aws_api_gateway_authorizer" "cognito_auth" {
+  name                    = "CognitoAuthorizer"
+  rest_api_id             = aws_api_gateway_rest_api.hello_api.id
+  identity_source         = "method.request.header.Authorization"
+  type                    = "COGNITO_USER_POOLS"
+  provider_arns           = [aws_cognito_user_pool.user_pool.arn]
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.hello_api.id
+  resource_id             = aws_api_gateway_resource.hello_resource.id
+  http_method             = aws_api_gateway_method.hello_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.hello_world.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "hello_deployment" {
+  depends_on = [aws_api_gateway_integration.lambda_integration]
+  rest_api_id = aws_api_gateway_rest_api.hello_api.id
+  stage_name  = "prod"
+}
