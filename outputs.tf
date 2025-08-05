d67676e60f27a4dc3d6e29aba5d3da510e996cea
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-06 03:21:54 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,4 @@
-output "s3_bucket_name" {
-  value = aws_s3_bucket.website_bucket.bucket
-}
 
-output "cloudfront_distribution_domain_name" {
-  value = aws_cloudfront_distribution.website_distribution.domain_name
-}
 
 output "cognito_user_pool_id" {
   value = aws_cognito_user_pool.user_pool.id
