c0e29c3f2c741386b8953d15f30d299101e686c0
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-05 14:29:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -203,5 +203,4 @@ resource "aws_api_gateway_integration" "lambda_integration" {
 resource "aws_api_gateway_deployment" "hello_deployment" {
   depends_on = [aws_api_gateway_integration.lambda_integration]
   rest_api_id = aws_api_gateway_rest_api.hello_api.id
-  stage_name  = "prod"
 }
