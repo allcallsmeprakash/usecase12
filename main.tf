7822eb9d215f4b97252e6d9501b48bd29de8a422
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-05 14:59:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -110,7 +110,7 @@ resource "aws_cognito_user_pool_client" "user_pool_client" {
   user_pool_id = aws_cognito_user_pool.user_pool.id
   generate_secret = false
   allowed_oauth_flows_user_pool_client = true
-  allowed_oauth_flows = ["code"]
+  allowed_oauth_flows = ["code", "implicit"]
   allowed_oauth_scopes = ["email", "openid", "profile"]
   callback_urls = ["https://hello-world-app-prod-domain.auth.us-east-1.amazoncognito.com/callback"]
   logout_urls   = ["https://hello-world-app-prod-domain.auth.us-east-1.amazoncognito.com/logout"]
